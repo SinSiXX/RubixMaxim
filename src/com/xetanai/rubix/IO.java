b00c9bee908f5eb4ac3cadf3c0d2bfa42f9cdcff
Crowdhopper <adencee@gmail.com>
2016-03-12 04:54:11 -0500

    IO Class added

---
+++ src/com/xetanai/rubix/IO.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class IO {
+   
+   public static Hashtable<String, String> read_a_at_b(String filename) throws FileNotFoundException {
+      Hashtable<String, String> retVal = new Hashtable<String, String>();
+      try {
+         File file = new File(filename);
+         Scanner input = new Scanner(file);
+         String line;
+         String[] split;
+         while(input.hasNextLine()) {
+            line = input.nextLine();
+            split = line.split("@");
+            retVal.put(split[0], split[1]);
+         }
+         input.close();
+      }
+      catch(FileNotFoundException e) {
+         System.out.println("File not found!");
+         e.printStackTrace();
+      }
+      catch(Exception e) {
+         e.printStackTrace();
+      }
+      return retVal;
+   }
+   
+}
